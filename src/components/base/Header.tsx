2bd36b864086054028adc8bf36f2f47c3ac296d6
Goran Mržljak <goran.mrzljak@gmail.com>
2020-12-20 22:30:12 +0100

    Extract header code to a separate component

---
+++ src/components/base/Header.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  createStyles,
+  IconButton,
+  StyleRules,
+  SvgIcon,
+  Theme,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
+import { Menu } from 'mdi-material-ui';
+import { useStyles } from '../../utils/ui-hooks';
+
+interface HeaderProps {
+  readonly onMenuOpen: () => void;
+}
+
+type ClassKey = 'toolbar' | 'menuButton' | 'title';
+
+function stylesCallback(theme: Theme): StyleRules<ClassKey, HeaderProps> {
+  return createStyles<ClassKey, HeaderProps>({
+    toolbar: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    }
+  });
+}
+
+export function Header(props: HeaderProps): React.ReactElement {
+  const { onMenuOpen } = props;
+
+  const classes = useStyles(props, stylesCallback);
+
+  return (
+    <header>
+      <Toolbar>
+        <IconButton
+          edge={'start'}
+          className={classes.menuButton}
+          color={'inherit'}
+          aria-label={'menu'}
+          onClick={onMenuOpen}
+        >
+          <SvgIcon component={Menu} fontSize={'large'} />
+        </IconButton>
+        <Typography variant={'h6'} className={classes.title}>
+          News
+        </Typography>
+      </Toolbar>
+    </header>
+  );
+}
--- src/components/base/Layout.tsx
+++ src/components/base/Layout.tsx
@@ -1,47 +1,18 @@
 import React, { CSSProperties, useCallback, useState } from 'react';
-import {
-  AppBar,
-  Container,
-  createStyles,
-  IconButton,
-  StyleRules,
-  SvgIcon,
-  Theme,
-  Toolbar,
-  Typography
-} from '@material-ui/core';
-import { useStyles } from '../../utils/ui-hooks';
+import { AppBar, Container, Toolbar } from '@material-ui/core';
 import { ScreenRoutes } from './ScreenRoutes';
-import { Menu } from 'mdi-material-ui';
 import { MainMenu } from './MainMenu';
 import { useHistory } from 'react-router-dom';
 import { History } from 'history';
+import { Header } from './Header';
 
 interface LayoutProps {}
-
-type ClassKey = 'toolbar' | 'menuButton' | 'title';
-
-function stylesCallback(theme: Theme): StyleRules<ClassKey, LayoutProps> {
-  return createStyles<ClassKey, LayoutProps>({
-    toolbar: {
-      flexGrow: 1
-    },
-    menuButton: {
-      marginRight: theme.spacing(2)
-    },
-    title: {
-      flexGrow: 1
-    }
-  });
-}
 
 const CONTAINER_STYLES: CSSProperties = {
   paddingTop: 10
 };
 
 export function Layout(props: LayoutProps): React.ReactElement {
-  const classes = useStyles(props, stylesCallback);
-
   const history: History = useHistory();
 
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
@@ -63,20 +34,7 @@ export function Layout(props: LayoutProps): React.ReactElement {
   return (
     <div>
       <AppBar position={'fixed'}>
-        <Toolbar>
-          <IconButton
-            edge={'start'}
-            className={classes.menuButton}
-            color={'inherit'}
-            aria-label={'menu'}
-            onClick={onMenuOpen}
-          >
-            <SvgIcon component={Menu} fontSize={'large'} />
-          </IconButton>
-          <Typography variant={'h6'} className={classes.title}>
-            News
-          </Typography>
-        </Toolbar>
+        <Header onMenuOpen={onMenuOpen} />
       </AppBar>
       <Toolbar />
       <MainMenu open={menuOpen} onClose={onMenuClose} onNavigate={onNavigate} />
