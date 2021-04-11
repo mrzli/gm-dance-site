c64cb5d6d8041afbb94f1fb97d1a42c621257f65
Goran Mržljak <goran.mrzljak@gmail.com>
2021-04-11 22:20:58 +0200

    fix to header

--- src/components/domain/FigureContainerHeader.tsx
+++ src/components/domain/FigureContainerHeader.tsx
@@ -4,11 +4,12 @@ import { FigureContainerHeaderItem } from './FigureContainerHeaderItem';
 export function FigureContainerHeader(): React.ReactElement {
   return (
     <>
-      <FigureContainerHeaderItem row={1} column={1} text={'Opis'} />
-      <FigureContainerHeaderItem row={1} column={2} text={'Početno držanje'} />
-      <FigureContainerHeaderItem row={1} column={3} text={'Završno držanje'} />
-      <FigureContainerHeaderItem row={1} column={4} text={'Napomena'} />
-      <FigureContainerHeaderItem row={1} column={5} text={'Video'} />
+      <FigureContainerHeaderItem row={1} column={1} text={'#'} />
+      <FigureContainerHeaderItem row={1} column={2} text={'Opis'} />
+      <FigureContainerHeaderItem row={1} column={3} text={'Početno držanje'} />
+      <FigureContainerHeaderItem row={1} column={4} text={'Završno držanje'} />
+      <FigureContainerHeaderItem row={1} column={5} text={'Napomena'} />
+      <FigureContainerHeaderItem row={1} column={6} text={'Video'} />
     </>
   );
 }
