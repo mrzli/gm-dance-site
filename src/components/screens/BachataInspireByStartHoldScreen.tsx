c322fae59a885d342a08a21b6005c31ae76975e0
Goran Mržljak <goran.mrzljak@gmail.com>
2021-04-11 22:42:19 +0200

    tiny update

--- src/components/screens/BachataInspireByStartHoldScreen.tsx
+++ src/components/screens/BachataInspireByStartHoldScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FigureByStartHoldContainer } from '../domain/figures-by-start-hold/FigureByStartHoldContainer';
+import { FigureByStartHoldContainer } from '../domain/figure-by-start-hold/FigureByStartHoldContainer';
 import figuresData from '../../data/figures-by-start-hold-data.json';
 
 export function BachataInspireByStartHoldScreen(): React.ReactElement {
