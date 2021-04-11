88a963ff713b291a581e7287eacda57ec193c5fa
Goran Mržljak <goran.mrzljak@gmail.com>
2021-04-11 20:33:46 +0200

    prevent running husky on heroku

--- scripts/src/postinstall.ts
+++ scripts/src/postinstall.ts
@@ -2,8 +2,10 @@ import { shell } from './util/util';
 import { getAppEnv } from './util/app-env';
 
 async function fn(): Promise<void> {
-  await shell(`echo aaa ${JSON.stringify(getAppEnv())}`);
-  //await shell('husky install');
+  const env = getAppEnv();
+  if (env.DEPLOYMENT !== 'true') {
+    await shell('husky install');
+  }
 }
 
 fn().finally();
