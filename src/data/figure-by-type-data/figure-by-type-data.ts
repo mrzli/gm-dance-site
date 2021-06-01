712a5f7fdacabbdc1c8cdfa66a2cb810e4d68ea2
Goran Mržljak <goran.mrzljak@gmail.com>
2021-06-01 21:22:08 +0200

    update

--- src/data/figure-by-type-data/figure-by-type-data.ts
+++ src/data/figure-by-type-data/figure-by-type-data.ts
@@ -1,8 +1,8 @@
 import { FigureByTypeSectionGroupData } from '../../types/domain/figure-by-type/figure-by-type-section-group-data';
 import { FIGURE_BY_TYPE_GROUP_BASIC_FIGURES } from './groups/basic-figures';
+import { FIGURE_BY_TYPE_SENSUAL } from './groups/sensual';
 import { FIGURE_BY_TYPE_GROUP_TURNS } from './groups/turns';
 import { FIGURE_BY_TYPE_GROUP_TWISTED } from './groups/twisted';
-import { FIGURE_BY_TYPE_SENSUAL } from './groups/sensual';
 import { FIGURE_BY_TYPE_NINETY_DEGREES_OR_ALIGNED } from './groups/ninety-degrees-or-aligned';
 import { FIGURE_BY_TYPE_BACK_TURNED_WOMEN } from './groups/back-turned-women';
 import { FIGURE_BY_TYPE_BACK_TURNED_MEN } from './groups/back-turned-men';
@@ -11,9 +11,9 @@ import { FIGURE_BY_TYPE_OTHER } from './groups/other';
 
 export const FIGURE_BY_TYPE_DATA: readonly FigureByTypeSectionGroupData[] = [
   FIGURE_BY_TYPE_GROUP_BASIC_FIGURES,
+  FIGURE_BY_TYPE_SENSUAL,
   FIGURE_BY_TYPE_GROUP_TURNS,
   FIGURE_BY_TYPE_GROUP_TWISTED,
-  FIGURE_BY_TYPE_SENSUAL,
   FIGURE_BY_TYPE_NINETY_DEGREES_OR_ALIGNED,
   FIGURE_BY_TYPE_BACK_TURNED_WOMEN,
   FIGURE_BY_TYPE_BACK_TURNED_MEN,
